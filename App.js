321587caa3335b1d6a3168d3c642ad79490b575f
JacobFrollo <159093866+JacobFrollo@users.noreply.github.com>
2025-11-24 13:42:20 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, StatusBar } from "react-native";
-import MapView from "react-native-maps";
+import MapView, {Marker} from "react-native-maps";
 import styles from "./styles";
 
 StatusBar.setBarStyle("dark-content");
@@ -14,12 +14,12 @@ export default function App() {
                 showsUserLocation
                 followUserLocation
             >
-                <MapView.Marker
+                <Marker
                     title="McAlister's Deli"
                     description="Sandwich Shop"
                     coordinate={{
-                        latitude: 39.95759084378544,
-                        longitude: -86.01729608825835,
+                        latitude: 39.95587108875202,
+                        longitude: -86.01999786764371,
                     }}
                 />
             </MapView>
